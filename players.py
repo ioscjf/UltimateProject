3ae67f4594e40d1d5a7c39aa9939720aefa2a243
tarees01 <tarees01@luther.edu>
2016-09-29 15:30:15 -0500

    Add files via upload

---
+++ players.py
@@ -0,0 +1,33 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+DB_name = "ultimate"
+
+TABLES = {}
+
+TABLES{"teams"} = (
+	"CREATE TABLE 'teams' ("
+	"'teamName' varchar(20) NOT NULL,"
+	"'school' varchar(20) NOT NULL,"
+	) 
+
+TABLES{"players"} = (
+        "CREATE TABLE 'players' ("
+        "'name' varchar(20) NOT NULL,"
+        "'school' varchar(20) NOT NULL,"
+        "'jersey number' varchar(20) NOT NULL,"
+        "'postion' varchar(20) NOT NULL,"
+        "'height' varchar(20) NOT NULL,"
+        "'weight' varchar(20) NOT NULL,"
+        "'biography' varchar(100) NOT NULL,"
+                )
+
+cnnn = mysql.connector.connect()
+cur = cnx.cursor()
+
+def create_database(cursor):
+	try:
+		cursoe.execute("CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
+	except mysql.connector.Error as err:
+		print("Failed creating database: {}".format(err))
+		exit(1)
