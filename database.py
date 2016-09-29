cb0802603a46c02cb1bb222927544b5aaa04c24e
Matt <smitma09@luther.edu>
2016-09-29 15:16:49 -0500

    Test

--- database.py
+++ database.py
@@ -7,6 +7,23 @@ TABLES = {}
 
 TABLES{"teams"} = (
 	"CREATE TABLE 'teams' ("
-	"'teamName' varchar(20) NOT NULL,"
+	"'teamName' varchar(40) NOT NULL,"
 	"'school' varchar(20) NOT NULL,"
+	"'competition division' varhar(10) NOT NULL,"
+	"'year' int(4),"
+	"'state' varchar(15),"
+	"'region' varchar(20),"
+	"'conference' varchar(20),"
+	"PRIMARY KEY ('teamName')"
 	)
+
+cnnn = mysql.connector.connect()
+cur = cnx.cursor()
+
+def create_database(cursor):
+	try:
+		cursoe.execute("CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
+	except mysql.connector.Error as err:
+		print("Failed creating database: {}".format(err))
+		exit(1)
+
