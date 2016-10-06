93fd143802de53a0368a3511ca833b4c7bf2f9ca
Connor Fitzpatrick <fitzco01@luther.edu>
2016-10-06 15:56:38 -0500

    Working on UI

---
+++ Ultimate Project/LoginViewController.swift
@@ -0,0 +1,51 @@
+//
+//  LoginViewController.swift
+//  Ultimate Project
+//
+//  Created by Connor Fitzpatrick on 10/6/16.
+//  Copyright © 2016 Connor Fitzpatrick. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBAction func back(_ sender: UIBarButtonItem) {
+        if self.presentingViewController != nil {
+            self.dismiss(animated: false, completion: nil)
+        }
+    }
+    
+    @IBOutlet weak var teamNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func newTeam(_ sender: UIButton) {
+    }
+    
+    // MARK: - Overrides
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
